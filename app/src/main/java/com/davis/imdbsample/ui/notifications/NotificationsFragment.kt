d90c8630449b36315a8e7549d176860ed9309b72
vis <hdronoh@gmail.com>
2021-10-27 01:18:45 +0300

    initial commit

---
+++ app/src/main/java/com/davis/imdbsample/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,26 @@
+package com.davis.imdbsample.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.davis.imdbsample.R
+
+class NotificationsFragment : Fragment() {
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+
+        return root
+    }
+}
